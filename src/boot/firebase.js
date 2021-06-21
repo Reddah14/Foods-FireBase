5f81a5d9c81d9f496ad6918ed05131b3fec5f680
Salvador Martinez <cartoonrocket9@gmail.com>
2021-06-21 12:22:08 +0200

    created BOOT file to trigger firebaseDb & set up initialization & config

---
+++ src/boot/firebase.js
@@ -0,0 +1,31 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+
+import "firebase/database"
+
+const firebaseConfig = {
+        apiKey: "AIzaSyAfGuYPQMImGi0ilu1KQbd_bPL1Z44ahiE",
+        authDomain: "foods-33e67.firebaseapp.com",
+        projectId: "foods-33e67",
+        storageBucket: "foods-33e67.appspot.com",
+        messagingSenderId: "518305107273",
+        appId: "1:518305107273:web:feea598abfb53013de1e98"
+  };
+  
+  // Initialize Firebase5
+  firebase.initializeApp(firebaseConfig);
+
+  // creating vars to store the access references to firebase database & authentication API's
+
+    let firebaseDb = firebaseApp.database()
+    let firebaseAuth = firebaseApp.auth()
+
+  // making available database and authentication API's
+
+    export { firebaseDb, firebaseAuth }
