2b159ee0d980c8aad2e61fdb73e63c35590f76c3
Salvador Martinez <cartoonrocket9@gmail.com>
2021-06-22 13:03:58 +0200

    Added ((|| Router Navigation Guards ||))

--- src/boot/router-auth.js
+++ src/boot/router-auth.js
@@ -1,15 +1,20 @@
 import { LocalStorage } from "quasar";
 
 export default ({ router }) => { // we pass router to manipulate it
-
   
-  router.beforeEach((to, from, next) => { // add hook beforeEach( fires before each navigation that user tries to do)
-    let userIsLogged = LocalStorage.getItem('isUserLoggedIn')
+    router.beforeEach((to, from, next) => { // add hook beforeEach( fires before each navigation that user tries to do)
+        let userIsLogged = LocalStorage.getItem('isUserLoggedIn')
+        console.log('to: ', to);    // logout params
+        console.log('from: ', from);
 
-    console.log('to: ', to);
-    console.log('from: ', from);
-    
-        next() // needed for app to start working again
+    // adding router Navigation Guards
+
+        if( !userIsLogged && to.path !== '/auth' ) {
+            // user not loggedIn go to login page
+            next('/auth') 
+        }
+        else { // user loggedIn
+            next() // go wherever
+        }
     })
-
 }
