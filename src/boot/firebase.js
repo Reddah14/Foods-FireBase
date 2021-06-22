2eb0008614d877651fbc4e1223fdd299e6a1948e
Salvador Martinez <cartoonrocket9@gmail.com>
2021-06-22 17:09:48 +0200

    FIX FireBase CONFIG !!

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -10,13 +10,14 @@ import "firebase/auth";
 import "firebase/database"
 
 const firebaseConfig = {
-        apiKey: "AIzaSyAfGuYPQMImGi0ilu1KQbd_bPL1Z44ahiE",
-        authDomain: "foods-33e67.firebaseapp.com",
-        projectId: "foods-33e67",
-        storageBucket: "foods-33e67.appspot.com",
-        messagingSenderId: "518305107273",
-        appId: "1:518305107273:web:feea598abfb53013de1e98"
-  };
+  apiKey: "AIzaSyAfGuYPQMImGi0ilu1KQbd_bPL1Z44ahiE",
+  authDomain: "foods-33e67.firebaseapp.com",
+  databaseURL: "https://foods-33e67-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "foods-33e67",
+  storageBucket: "foods-33e67.appspot.com",
+  messagingSenderId: "518305107273",
+  appId: "1:518305107273:web:feea598abfb53013de1e98"
+};
   
   // Initialize Firebase5
 let firebaseApp = firebase.initializeApp(firebaseConfig);
